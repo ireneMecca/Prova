79e692b24f064d3bc149731d43e10f39c5b6f819
irenemecca <irene.mecca@studenti.unibas.it>
2025-04-06 22:01:16 +0200

    modifica Costanti

--- Agenzia/src/it/unibas/agenzia/modello/Costanti.java
+++ Agenzia/src/it/unibas/agenzia/modello/Costanti.java
@@ -4,7 +4,6 @@ public class Costanti {
     
     public static final String TIPOLOGIA_CROCIERA = "Crociera";
     public static final String TIPOLOGIA_VILLAGGIO = "Villaggio";
-    public static final String TIPOLOGIA_CITTA = "Citta'";
     
     public static final String SI_ONLINE =  "Offre prenotazioni online";
     public static final String NO_ONLINE =  "Non offre prenotazioni online";
